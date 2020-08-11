11e9e0f6b20b781eae53c393575badecf4c733f6
Predictor Librus-Quattuor <51878699+PredictorLQ@users.noreply.github.com>
2020-08-11 23:48:43 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var radius = 107;
 var answer = {};
 var flag_count = 1;
-var colors = ["rgb(100%, 0%, 0%)","rgb(0%, 0%, 60%)","rgb(11%, 11%, 70%)","rgb(100%, 100%, 0%)"];
+var colors = ["rgb(100%, 0%, 0%)","rgb(0%, 60%, 60%)","rgb(11%, 11%, 70%)","rgb(100%, 100%, 0%)"];
 var range = 0;
 Number.prototype.toRad = function() { return this * Math.PI / 180; }
 $(document).ready(function() {
@@ -25,11 +25,12 @@ $(document).ready(function() {
         if (flag_count == colors.length) { 
             $(".question2").css("display", "none");
             $(".result").css("display", "block");
-            flag_count =0;
+            flag_count =1;
         }
         $("#question1").text("Вопрос "+ flag_count);
-        console.log(flag_count);
-        console.log(colors[flag_count-2]);
+        console.log("flag_count / "+flag_count);
+        console.log("flag_count - 2/ "+flag_count-2);
+        console.log("colors / "+colors[flag_count-2]);
         $("#anger-div").css("background", colors[flag_count-2]);
         range =0;
         $("#customRange1").trigger("input");
